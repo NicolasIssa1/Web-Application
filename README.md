7e3e2d8f69f2866f3d8324189acb3559b85e141c
NicolasIssa1 <112622885+NicolasIssa1@users.noreply.github.com>
2024-10-16 19:08:49 +0000

    done with all sections

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web-Application
+
+for now
+to run home page control clikc on the link
+to run the fruit page add /fruit
---
+++ Section_2/db_create.py
@@ -0,0 +1,6 @@
+from config import SQLALCHEMY_DATABASE_URI
+from app import db, app
+import os.path
+
+with app.app_context():
+    db.create_all()
